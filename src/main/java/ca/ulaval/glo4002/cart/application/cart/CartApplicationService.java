1bd581234a40e2ad61fe9382c031dada5d817a41
Samuel Baillargeon <samuel.baillargeon.1@ulaval.ca>
2018-11-21 20:36:34 -0500

    fix try catch

--- src/main/java/ca/ulaval/glo4002/cart/application/cart/CartApplicationService.java
+++ src/main/java/ca/ulaval/glo4002/cart/application/cart/CartApplicationService.java
@@ -24,7 +24,7 @@ public class CartApplicationService {
             List<Cart> carts = cartRepository.listCarts();
             return getCartByOwner(email, carts);
         } catch (Exception e) {
-	        throw new CannotFindCartException("Cannot find cart for " + email);
+	        throw new CannotFindCartException("Cannot find cart for " + email, e);
         }
 	}
 
